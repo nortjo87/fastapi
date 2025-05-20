50dca40fe51e7815206a9e2424a40010d3d5c7e1
nortjo87 <snyilhm@gmail.com>
2025-05-20 17:34:28 +0700

    1st-commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional, List, Dict, Any, Union
+from enum import Enum
+import pymongo
+
+application = FastAPI()
+
+class methodType(str, Enum):
+    credit = "credit"
+    debit = "debit"
+    cash = "cash"
+
+class Item(BaseModel):
+    name: str
+    price: Optional[float] = None
+    method: methodType
+
+    
+item_transaction = []
+
+# Quarry Parameters
+@application.get("/transaction")
+async def get_transaction(tipe:Optional[methodType] = None,):
+    if tipe is None:
+        return item_transaction
+    filtered_items = [item for item in item_transaction if item.method == tipe]
+    return filtered_items
+    
+
+# path parameters
+@application.get("/transaction/{name}/{amount}")
+async def get_transaction(name: str, amount: float):
+    return f"Transaction for {name} of amount {amount} processed successfully."
+
+@application.post("/transaction/")
+async def post_transaction(input_transaction: Item):
+    item_transaction.append(input_transaction)
+    return item_transaction
